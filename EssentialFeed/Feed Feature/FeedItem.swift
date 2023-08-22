4d0b374c47bdda0c735a01e96bfb3f3a514466e2
azubreiciuc <zubrlc@gmail.com>
2023-08-22 17:45:42 +0300

    Delivers items array on 200 HTTP response with JSON item

--- EssentialFeed/Feed Feature/FeedItem.swift
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -1,15 +1,28 @@
 //
 //  FeedItem.swift
 //  EssentialFeed
-//
-//  Created by azubreiciuc on 17.08.2023.
-//
 
 import Foundation
 
 public struct FeedItem: Equatable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let imageURL: URL
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
+
+extension FeedItem: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case description
+        case location
+        case imageURL = "image"
+    }
 }
